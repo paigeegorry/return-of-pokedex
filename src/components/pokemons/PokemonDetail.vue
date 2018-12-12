1521b35e7f5ac3c8a47353614d17b15d06e8719e
Paige Gorry <paigeegorry@gmail.com>
2018-12-12 15:17:21 -0800

    added more details to the pokemondetail component

--- src/components/pokemons/PokemonDetail.vue
+++ src/components/pokemons/PokemonDetail.vue
@@ -1,6 +1,19 @@
 <template>
     <section v-if="pokemon">
         <h2>{{pokemon.pokemon}}</h2>
+        <p>height: {{pokemon.height}} |
+            weight: {{pokemon.weight}}</p>
+        <p>shape: {{pokemon.shape}}</p>
+        <p>attack: {{pokemon.attack}} |
+            defense: {{pokemon.defense}}</p>
+        <p>egg group 1: {{pokemon.egg_group_1}}</p>
+        <p>egg group 2: {{pokemon.egg_group_2}}
+        <p>ability: {{pokemon.ability_1}}</p>
+        <p>hidden ability: 
+            <span> 
+              {{pokemon.ability_hidden}}
+            </span>
+        </p>
         <a :href="pokemon.pokedex">Learn more.</a>
     </section>
 </template>
@@ -13,6 +26,25 @@ export default {
 };
 </script>
 
-<style lang="postcss">
-
+<style lang="postcss" scoped>
+section {
+  padding: 50px;
+  text-align: center;
+  margin: 20px;
+  font-family: 'Courier New', Courier, monospace;
+  font-size: 1.5em;
+  p {
+    padding: 10px;
+  }
+  span {
+    visibility: hidden;
+  }
+  span:hover {
+    visibility: visible;
+  }
+  a:hover {
+    font-weight: bold;
+  }
+  
+}
 </style>
